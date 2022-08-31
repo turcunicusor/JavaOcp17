5a912271734fc05898adf32cc9ccdb0c006556f5
Nicusor Turcu <nicusor.turcu@mambu.com>
2022-08-31 10:20:58 +0300

    31 August, chapter 1.

---
+++ chapter1/src/Animal.java
@@ -0,0 +1,47 @@
+public class Animal {
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// method
+
+	public int numberVisitors(int month) {
+		return 10;
+	}
+
+	/* Multiple
+	 * line comment
+	 */
+
+	/* Multiple
+	   line comment
+	 */
+
+	/**
+	 * Javadoc multiple-line comment
+	 * @author Jeanne and Scott
+	 */
+
+	/*
+	 * // anteater
+	 */
+
+	// bear
+
+	// // cat
+
+	// /* dog */
+
+	/* elephant */
+	/*
+	 * /* ferret */
+ 	*/
+	// compiler sees /* */ */
+
+}
