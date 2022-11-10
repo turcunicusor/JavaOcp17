75ce826622108536606b5fe058f987c1cdcf8f9f
Nicusor Turcu <nicusor.turcu@mambu.com>
2022-11-10 17:01:35 +0200

    10 Nov, chapter 1 - questions

---
+++ chapter1-buildingblocks/src/Identifiers.java
@@ -0,0 +1,19 @@
+public class Identifiers {
+
+	public static String classvar;
+	public String instance;
+
+	public static void main(String[] x){
+//		int _ = 1;
+		int _hellowold$ = 1;
+//		int true = 1;
+//		int java.lang = 1;
+		int Public = 1;
+//		int 1980_s = 1;
+		int _Q2_ = 1;
+		System.out.print(classvar);
+
+		var xamp = new Identifiers();
+		System.out.print(xamp.instance);
+	}
+}
